144b2c6719e786300e29a865d9492b19f03a11a4
hasan-furkan <81443587+hasan-furkan@users.noreply.github.com>
2022-06-05 12:57:10 +0300

    finish_method

---
+++ FinishMethod/app/src/main/java/com/hasanfurkan/finishmethod/ActivitySonuc.kt
@@ -0,0 +1,13 @@
+package com.hasanfurkan.finishmethod
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitySonuc : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sonuc)
+    }
+
+
+}
