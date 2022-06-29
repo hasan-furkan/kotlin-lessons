5f19717b27f6ede43a23d366930c462e994e1802
hasan-furkan <81443587+hasan-furkan@users.noreply.github.com>
2022-06-29 16:14:58 +0300

    tab_layout_kullanimi

---
+++ TabLayoutKullanimi/app/src/main/java/com/hasanfurkan/tablayoutkullanimi/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.hasanfurkan.tablayoutkullanimi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val fragmentListesi = ArrayList<Fragment>()
+    private val fragmentBaslikListesi = ArrayList<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fragmentListesi.add(FragmentBirinci())
+        fragmentListesi.add(FragmentIkinci())
+        fragmentListesi.add(FragmentUcuncu())
+
+        val adapter = MyViewPagerAdapter(this)
+
+        viewPager2.adapter = adapter
+
+
+        fragmentBaslikListesi.add("Bir")
+        fragmentBaslikListesi.add("Iki")
+        fragmentBaslikListesi.add("Uc")
+
+        TabLayoutMediator(tablayout, viewPager2){tab, position ->
+            tab.setText(fragmentBaslikListesi[position])
+        }.attach()
+
+        tablayout.getTabAt(0)!!.setIcon(R.drawable.resim)
+
+    }
+
+    inner class MyViewPagerAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity){
+        override fun getItemCount(): Int {
+            return fragmentListesi.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+
+            return fragmentListesi[position]
+
+        }
+
+    }
+}
